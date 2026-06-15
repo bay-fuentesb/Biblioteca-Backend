c94fbb03e3ccd37010d8cbbe2d4ed0bf5c40e4b3
bay-fuentesb <bay.fuentesb@duocuc.cl>
2026-06-14 22:53:29 -0400

    build front

--- Frontend/src/environments/environment.development.ts
+++ Frontend/src/environments/environment.development.ts
@@ -2,14 +2,14 @@ export const environment = {
   production: false,
   
   // Puerto 4545: Microservicio de Anuncios
-  "urlAnuncio": "http://localhost:4545/anuncios",
+  "urlAnuncio": "http://52.21.160.178:4545/anuncios",
   
   // Puerto 8081: Microservicio de Libros (Corregido a plural /libros)
-  "urlLibro": "http://localhost:8081/libros",
+  "urlLibro": "http://52.21.160.178:8081/libros",
   
   // Puerto 8082: Microservicio de Reservas (Corregido a plural /reservas)
-  "urlReserva": "http://localhost:8082/reservas",
+  "urlReserva": "http://52.21.160.178:8082/reservas",
   
   // Puerto 8083: Microservicio de Multas (Corregido a plural /multas)
-  "urlMulta": "http://localhost:8083/multas"
+  "urlMulta": "http://52.21.160.178:8083/multas"
 };
